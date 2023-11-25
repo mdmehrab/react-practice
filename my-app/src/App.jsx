f9696ae2b611be2535d6ff5c2b8d0d9c26b883bc
rakib <ishraqmehrab2002@gmail.com>
2023-11-25 14:36:46 +0600

    dev sdosdosd

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,65 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1> hello world </h1>{' '}
-      <p>
-        {' '}
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.Impedit dolorem
-        fugit ad natus ullam, veritatis laboriosam ducimus cum ? Voluptatem
-        quidem incidunt eum enim eos minus quos id suscipit, debitis laborum
-        blanditiis numquam doloremque nostrum corporis.Voluptatum dolores
-        temporibus ipsa nam voluptas earum corporis.Perspiciatis nobis a
-        accusamus nisi similique at harum non cumque cum suscipit unde provident
-        maiores consequuntur perferendis pariatur fugiat exercitationem magni
-        quod ab, voluptatem debitis sed assumenda qui!Quasi assumenda architecto
-        similique delectus, iusto animi saepe eum dolore necessitatibus sit
-        nesciunt.Qui, adipisci.Repudiandae, nam omnis laborum nisi distinctio
-        unde iusto officiis asperiores error possimus consectetur, est itaque
-        minus similique, atque consequatur eligendi maxime illum nihil obcaecati
-        quis saepe!Dolore deleniti amet consequatur perspiciatis illo
-        necessitatibus recusandae placeat facere, nam est quasi laudantium, enim
-        veniam!Nesciunt eum quam delectus.Tenetur ullam dolor ipsum totam, quas
-        molestiae magnam quasi facere sint perferendis commodi nisi eum error
-        voluptatum illum tempora veritatis.Atque, minima ? Culpa tenetur,
-        blanditiis vitae ipsa unde reprehenderit nam facilis non eveniet ipsum
-        sit cum itaque sequi earum asperiores corporis recusandae.Laboriosam
-        modi doloribus delectus a repudiandae atque excepturi earum soluta
-        quidem dicta ab nesciunt, incidunt eveniet cum tempore debitis dolore ?
-        Sit iure adipisci consectetur fuga deserunt qui sed ex quia natus
-        inventore vero non nihil, explicabo sint sapiente reiciendis
-        nam.Incidunt, tempora, repellat totam aperiam, inventore molestiae non
-        dolorem nam accusamus dolore illum tenetur ullam soluta perferendis esse
-        ? Cumque enim iste qui laboriosam saepe facilis ? Odio ducimus hic ab,
-        dolorum beatae magnam voluptatem labore cumque voluptates blanditiis
-        sunt similique necessitatibus quae asperiores at ullam voluptate a
-        suscipit reprehenderit voluptatum alias possimus perferendis autem!Quae,
-        totam, voluptate consequatur quia voluptatibus repellendus voluptatum
-        provident excepturi delectus tenetur numquam ad pariatur, natus iure
-        vero dolores alias eligendi officiis labore nulla quisquam velit!Libero
-        id vel eveniet ad provident!Illum vel et voluptatibus dolores voluptatem
-        a sapiente, consectetur possimus ex provident unde earum culpa
-        tempora!Expedita repudiandae perferendis rem eveniet impedit soluta
-        porro magni ut excepturi esse!Eos ducimus aperiam placeat magni,
-        accusantium saepe sed unde nam, inventore deleniti perspiciatis
-        blanditiis numquam distinctio repudiandae quibusdam, necessitatibus quod
-        quo!Tenetur et ut quam illo.Quae accusamus exercitationem laborum, sed
-        sunt dignissimos aliquid, esse culpa assumenda ex necessitatibus nemo
-        doloribus corporis nisi beatae eius atque quod accusantium impedit quis
-        eaque excepturi optio ut odio.Numquam in praesentium obcaecati nostrum
-        laudantium a quo quia repellat placeat nisi dolorem magnam voluptatibus
-        necessitatibus deleniti corporis, aliquam dolorum totam quaerat
-        delectus!Repudiandae alias porro laboriosam unde, facilis laudantium, ad
-        iste consequuntur enim, quis adipisci.Exercitationem rerum magni
-        voluptates soluta, magnam odit ratione sit expedita nam dolore assumenda
-        tenetur nesciunt esse officia, enim sunt, delectus recusandae sed minus
-        fugit tempora unde iusto mollitia!Necessitatibus dolor fuga
-        reprehenderit eius dolorum, rerum enim nemo exercitationem culpa optio
-        veritatis asperiores in blanditiis!Nemo quae facere consectetur
-        perspiciatis labore quibusdam doloremque quam voluptate impedit dolorum
-        voluptatum nostrum debitis expedita ipsa sunt tempora ducimus error,
-        maiores quaerat, excepturi vitae necessitatibus dolor.Vel laboriosam
-        blanditiis soluta.Dolores iure quo cum doloribus quas praesentium ?{' '}
-      </p>{' '}
+      <h1> this is dev branch </h1>{' '}
     </div>
   );
 };
